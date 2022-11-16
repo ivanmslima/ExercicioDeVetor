603cc01fa904e8af5eea4bf1e6d4be71df058fa7
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-16 20:05:16 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioDeVetor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+
+            int N;
+            int[] vet;
+            Console.Write("Primeiro digite a quantidade de números que vai digitar, em seguida o programa irá separar os números negativos: ");
+
+
+            N = int.Parse(Console.ReadLine());
+            vet = new int[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i= 0; i < N; i++)
+            {
+                vet[i] = int.Parse(s[i]);
+            }
+
+            for (int i=0; i < N; i++)
+            {
+                if (vet[i] < 0)
+                {
+                    Console.WriteLine(vet[i]);
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
